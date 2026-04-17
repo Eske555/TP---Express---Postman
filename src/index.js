d37a4780d79597d52e176993d323c9012e017ecd
IGNACIO ESKENAZI <49374119@ort.edu.ar>
2026-04-17 10:14:43 -0300

    Pequeñas correcciones

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { sumar, restar, multiplicar, dividir } from "./modules/matematica.js";
+
 import Alumno from "./models/alumno.js";
+import { sumar, restar, multiplicar, dividir } from "./modules/matematica.js";
 import { OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID } from "./modules/omdb-wrapper.js";
 
 
